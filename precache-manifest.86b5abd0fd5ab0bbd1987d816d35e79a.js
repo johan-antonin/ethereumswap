134f680c6ac62370218a53b5539d8526353a55bd
johan.antonin <johan.antonin@gmail.com>
2021-05-24 23:12:19 +0200

    Updates

---
+++ precache-manifest.86b5abd0fd5ab0bbd1987d816d35e79a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4dfe709a115043e0f9ac",
+    "url": "/ethereumswap/static/css/main.bc4588a5.chunk.css"
+  },
+  {
+    "revision": "4dfe709a115043e0f9ac",
+    "url": "/ethereumswap/static/js/main.b4e5cda4.chunk.js"
+  },
+  {
+    "revision": "dcd1b1467313cf7d640d",
+    "url": "/ethereumswap/static/js/runtime~main.1cd83912.js"
+  },
+  {
+    "revision": "07f4ae3e6e83b44038a2",
+    "url": "/ethereumswap/static/css/2.4b889bce.chunk.css"
+  },
+  {
+    "revision": "07f4ae3e6e83b44038a2",
+    "url": "/ethereumswap/static/js/2.67f6e688.chunk.js"
+  },
+  {
+    "revision": "6f6ad4e26d2bf1d57b638a01c85e3976",
+    "url": "/ethereumswap/static/media/logoiata.6f6ad4e2.png"
+  },
+  {
+    "revision": "45f361e85255ce9af3e9333aee219cbc",
+    "url": "/ethereumswap/index.html"
+  }
+];
